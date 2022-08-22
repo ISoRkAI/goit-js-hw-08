3fc29b62d8863acef599e71f3627875b44de129e
Dmytro Soroka <dimasoroka17@gmail.com>
2022-08-22 22:33:48 +0300

    tasc-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector(".feedback-form input"),
+    textarea: document.querySelector(".feedback-form textarea"),
+}
+
+refs.form.addEventListener("submit", throttle(onFormSubmit, 500));
+refs.form.addEventListener("input", throttle(onTextareaInput, 500));
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+populateMessage()
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+}
+
+function onTextareaInput(evt) {
+    if (evt.target.value !== "") {
+        formData[evt.target.name] = evt.target.value;
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    }
+}
+
+function populateMessage() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY)
+    
+    if (savedMessage) {
+        const savedMessagePars = JSON.parse(savedMessage);
+
+        if (savedMessagePars['email'] !== undefined) {
+            refs.input.value = savedMessagePars['email'];
+
+        } else {
+             refs.input.value = '';
+        }
+        if (savedMessagePars['message'] !== undefined) {
+             refs.textarea.value = savedMessagePars['message'];
+
+        } else {
+             refs.textarea.value = '';
+        }
+    }
+}
